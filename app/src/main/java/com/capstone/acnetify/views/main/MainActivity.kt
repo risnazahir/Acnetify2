7078b7fa7e7662143009bf028623065b4021d44f
kisahtegar <code.kisahtegar@gmail.com>
2024-06-02 00:26:19 +0700

    Working on CameraActivity adding permission.

--- app/src/main/java/com/capstone/acnetify/views/main/MainActivity.kt
+++ app/src/main/java/com/capstone/acnetify/views/main/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.capstone.acnetify.views.main
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.capstone.acnetify.R
 import com.capstone.acnetify.databinding.ActivityMainBinding
@@ -17,12 +22,49 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    /**
+     * Permission request launcher for camera permission.
+     *
+     * This launcher handles the result of the camera permission request. It displays a toast
+     * message indicating whether the permission request was granted or denied.
+     */
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Display a toast message indicating that the permission request was granted
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_LONG).show()
+            } else {
+                // Display a toast message indicating that the permission request was denied
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+    /**
+     * Checks if all required permissions are granted.
+     *
+     * This function checks whether the camera permission is granted to the application.
+     * @return true if the camera permission is granted, false otherwise.
+     */
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.bottomNavigationView.background = null
+
+        // Request camera permission if not granted
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
 
         // Set the initial fragment
         if (savedInstanceState == null) {
@@ -64,4 +106,8 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
+    companion object {
+        // Required camera permission constant
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
+    }
 }
