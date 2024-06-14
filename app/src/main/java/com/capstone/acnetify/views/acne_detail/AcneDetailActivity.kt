0601fce46dbb03d0d4129b1fa5c15fabe8ad3119
kisahtegar <code.kisahtegar@gmail.com>
2024-06-14 20:49:52 +0700

    Implementing detail page for showing image, acne type and description.

--- app/src/main/java/com/capstone/acnetify/views/acne_detail/AcneDetailActivity.kt
+++ app/src/main/java/com/capstone/acnetify/views/acne_detail/AcneDetailActivity.kt
@@ -1,8 +1,17 @@
 package com.capstone.acnetify.views.acne_detail
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.capstone.acnetify.R
+import com.capstone.acnetify.data.model.ImageSubmissionsModel
 import com.capstone.acnetify.data.model.ReviewsModel
 import com.capstone.acnetify.databinding.ActivityAcneDetailBinding
 import com.capstone.acnetify.views.home.ReviewsAdapter
@@ -17,24 +26,52 @@ class AcneDetailActivity : AppCompatActivity() {
         binding = ActivityAcneDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Retrive detail object from the intent extras
+        val acneDetail = if (Build.VERSION.SDK_INT >= 33) {
+            intent.getParcelableExtra(EXTRA_ACNE_ITEM, ImageSubmissionsModel::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra(EXTRA_ACNE_ITEM)
+        }
+        Log.d(LOG, "acneDetail: $acneDetail")
+
+        acneDetail?.let {
+            Glide.with(this)
+                .load(it.imageUrl)
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_error))
+                .into(binding.previewImageView)
+
+            binding.textViewAcneType.text = when (it.acneType) {
+                "acne_nodules" -> "Nodules"
+                "milia" -> "Milia"
+                "blackhead" -> "Blackhead"
+                "whitehead" -> "Whitehead"
+                "papula_pustula" -> "Papula & Pustula"
+                else -> "Unknown Acne Type"
+            }
+            binding.textViewDescription.text = when (it.acneType) {
+                "acne_nodules" -> getString(R.string.acne_nodule_desc)
+                "milia" -> getString(R.string.acne_milia_desc)
+                "blackhead" -> getString(R.string.acne_blackhead_desc)
+                "whitehead" -> getString(R.string.acne_whitehead_desc)
+                "papula_pustula" -> getString(R.string.acne_papula_pustula_desc)
+                else -> ""
+            }
+        }
+
         binding.rvReviews.apply {
             layoutManager = LinearLayoutManager(context)
             setHasFixedSize(true)
             adapter = feedsAdapter
         }
 
-        // Provide mock data
-        //feedsAdapter.submitList(getMockData())
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
     }
 
-    private fun getMockData(): List<ReviewsModel> {
-        return listOf(
-            ReviewsModel("1", "kisahtegar", 31491, "Acne-A", "2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in scelerisque sem. Mauris", 2, true),
-            ReviewsModel("1", "kisahtegar", 31491, "Acne-A", "2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in scelerisque sem. Mauris", 2, true),
-            ReviewsModel("1", "kisahtegar", 31491, "Acne-A", "2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in scelerisque sem. Mauris", 2, true),
-            ReviewsModel("1", "kisahtegar", 31491, "Acne-A", "2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in scelerisque sem. Mauris", 2, true),
-            ReviewsModel("1", "kisahtegar", 31491, "Acne-A", "2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in scelerisque sem. Mauris", 2, true),
-            ReviewsModel("1", "kisahtegar", 31491, "Acne-A", "2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in scelerisque sem. Mauris", 2, true),
-        )
+    companion object {
+        private val LOG = AcneDetailActivity::class.java.simpleName
+        const val EXTRA_ACNE_ITEM = "extra_acne_item"
     }
 }
